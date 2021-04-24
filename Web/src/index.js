aa26b2e98ea5b41c0d0d99b2fbfaea0d44fa00d8
elisvcodes <sultanoveli@outlook.com>
2021-04-24 14:48:39 -0400

    Updated App.js to use redux

--- Web/src/index.js
+++ Web/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './_Reducers/index';
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
