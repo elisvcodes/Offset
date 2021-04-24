4b535e385713bd40746498937319492537cc7633
NelsonWang <wangnelson2@gmail.com>
2021-04-24 03:17:32 -0400

    Overview | Filter out

--- Mobile/api_sustainability/lib/screens/tracker_screen.dart
+++ Mobile/api_sustainability/lib/screens/tracker_screen.dart
@@ -20,7 +20,7 @@ class TrackerScreen extends StatefulWidget {
 }
 
 class _TrackerScreenState extends State<TrackerScreen> {
-  var _showOnlyFavorites = false;
+  var _showOnlyFavorites = true;
   var _isInit = true;
   var _isLoading = false;
 
@@ -54,46 +54,7 @@ class _TrackerScreenState extends State<TrackerScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Market Place'),
-        actions: <Widget>[
-          PopupMenuButton(
-            onSelected: (FilterOptions selectedValue) {
-              setState(() {
-                if (selectedValue == FilterOptions.Favorites) {
-                  _showOnlyFavorites = true;
-                } else {
-                  _showOnlyFavorites = false;
-                }
-              });
-            },
-            icon: Icon(
-              Icons.more_vert,
-            ),
-            itemBuilder: (_) => [
-              PopupMenuItem(
-                child: Text('Show Tracked Items'),
-                value: FilterOptions.Favorites,
-              ),
-              PopupMenuItem(
-                child: Text('Show All'),
-                value: FilterOptions.All,
-              ),
-            ],
-          ),
-          Consumer<Cart>(
-            builder: (_, cart, ch) => Badge(
-              child: ch,
-              value: cart.itemCount.toString(),
-            ),
-            child: IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(CartScreen.routeName);
-              },
-            ),
-          ),
-        ],
+        actions: <Widget>[],
       ),
       drawer: AppDrawer(),
       body: _isLoading
