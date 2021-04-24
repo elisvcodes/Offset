3c30cfd766e131c9a2752b532436ce948741f497
NelsonWang <wangnelson2@gmail.com>
2021-04-24 01:18:26 -0400

    Overview | chart display | Interactive

--- Mobile/api_sustainability/lib/screens/product_detail_screen.dart
+++ Mobile/api_sustainability/lib/screens/product_detail_screen.dart
@@ -5,6 +5,8 @@ import 'dart:math' as math;
 import 'package:fl_chart/fl_chart.dart';
 import '../providers/products.dart';
 import '../providers/product.dart';
+
+import 'package:intl/intl.dart';
 import '../providers/auth.dart';
 
 math.Random random = new math.Random();
@@ -46,6 +48,10 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     // final product = Provider.of<Product>(context, listen: false);
     final authData = Provider.of<Auth>(context, listen: false);
     var data = _generateCarbonData(10);
+    final flSpotData = List.generate(
+            101, (i) => _carbonUsage * (i / _lifeTimeValue).round() / 10000)
+        .map((x) => FlSpot(x / 3, (x)))
+        .toList();
     final productId =
         ModalRoute.of(context).settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
@@ -167,6 +173,95 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                   pointColor: Colors.amber,
                 ),
               ),
+              Text("Emisions Graph",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  )),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 22.0, bottom: 20),
+                  child: SizedBox(
+                    width: 300,
+                    height: 200,
+                    child: LineChart(
+                      LineChartData(
+                        lineTouchData: LineTouchData(
+                          touchTooltipData: LineTouchTooltipData(
+                              maxContentWidth: 100,
+                              tooltipBgColor: Colors.orange,
+                              getTooltipItems: (touchedSpots) {
+                                return touchedSpots
+                                    .map((LineBarSpot touchedSpot) {
+                                  final textStyle = TextStyle(
+                                    color: touchedSpot.bar.colors[0],
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  );
+                                  return LineTooltipItem(
+                                      '${touchedSpot.x}, ${touchedSpot.y.toStringAsFixed(2)}',
+                                      textStyle);
+                                }).toList();
+                              }),
+                          handleBuiltInTouches: true,
+                          getTouchLineStart: (data, index) => 0,
+                        ),
+                        lineBarsData: [
+                          LineChartBarData(
+                            colors: [
+                              Colors.black,
+                            ],
+                            spots: flSpotData,
+                            isCurved: true,
+                            isStrokeCapRound: true,
+                            barWidth: 3,
+                            belowBarData: BarAreaData(
+                              show: false,
+                            ),
+                            dotData: FlDotData(show: false),
+                          ),
+                        ],
+                        minY: -1.5,
+                        maxY: 4,
+                        titlesData: FlTitlesData(
+                          leftTitles: SideTitles(
+                            showTitles: true,
+                            getTextStyles: (value) => const TextStyle(
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                            margin: 16,
+                          ),
+                          rightTitles: SideTitles(showTitles: false),
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            getTextStyles: (value) => const TextStyle(
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                            margin: 16,
+                          ),
+                          topTitles: SideTitles(showTitles: false),
+                        ),
+                        gridData: FlGridData(
+                          show: true,
+                          drawHorizontalLine: true,
+                          drawVerticalLine: true,
+                          horizontalInterval: 1.5,
+                          verticalInterval: 5,
+                          checkToShowHorizontalLine: (value) {
+                            return value.toInt() == 0;
+                          },
+                          checkToShowVerticalLine: (value) {
+                            return value.toInt() == 0;
+                          },
+                        ),
+                        borderData: FlBorderData(show: false),
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ]),
           ),
         ],
