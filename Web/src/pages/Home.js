ecc95596d5c8bc1cb5c8c91493f6b43ae7dc940e
elisvcodes <sultanoveli@outlook.com>
2021-04-23 20:44:27 -0400

    Started on the homepage

--- Web/src/pages/Home.js
+++ Web/src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
-
+import Layout from '../components/Layout/index';
+import { Container, Jumbotron, Button } from 'react-bootstrap';
 export default function Home() {
   return (
     <Layout>
-      <h1> Hello</h1>
+      <Jumbotron style={{ background: '#f8f9fa' }} className="text-center">
+        <Container>
+          <h1>MyCarbo!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Container>
+      </Jumbotron>
     </Layout>
   );
 }
