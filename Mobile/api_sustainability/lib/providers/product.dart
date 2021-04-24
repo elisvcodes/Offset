c031e1399517b25e9a7e249b8c5747dd70f095b7
NelsonWang <wangnelson2@gmail.com>
2021-04-23 20:56:04 -0400

    Flutter | Overview Screen

---
+++ Mobile/api_sustainability/lib/providers/product.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+// optimistic approach
+  void toggleFavoriteStatus(String authToken, String userId) async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    final url = Uri.parse(
+        'https://descartable-server-default-rtdb.firebaseio.com/usersFavorites/$userId/$id.json?auth=$authToken');
+    try {
+      final response = await http.put(
+        url,
+        body: json.encode(
+          isFavorite,
+        ),
+      );
+      // If there is an error but theconnection works
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      // If the attempts fails
+      _setFavValue(oldStatus);
+    }
+  }
+}
