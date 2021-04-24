aeed93431ff690c384abd7142353aa2d4a7fca07
elisvcodes <sultanoveli@outlook.com>
2021-04-24 14:47:22 -0400

    Improved the profile page

--- Web/src/pages/Profile/index.js
+++ Web/src/pages/Profile/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Table } from 'react-bootstrap';
 import Layout from '../../components/Layout/index';
+import profilePic from '../../media/1_PgIo7r6qQXem8BmWd-vksQ.jpeg';
 export default function Profile() {
   return (
     <>
       <Layout>
         <Container className="mt-5">
           <Row>
-            <Col>
-              <div></div>
+            <Col sm={4}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={profilePic} />
+                <Card.Body>
+                  <Card.Title>Susen Reid</Card.Title>
+                  <Card.Text>
+                    LEVEL: Gold
+                    <br /> Some quick example text to build on the card title
+                    and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col>
-              <h2> Items I am tracking</h2>
+            <Col sm={8}>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Item</th>
+                    <th>Tracking Since</th>
+                    <th>CO2 Preserved</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
             </Col>
           </Row>
         </Container>
