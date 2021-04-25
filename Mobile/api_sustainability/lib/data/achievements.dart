86e931cbe70f16aff0e4ef2130f7e8c33b84f3b9
NelsonWang <wangnelson2@gmail.com>
2021-04-25 07:49:55 -0400

    Fluter | Profile | Achievements

---
+++ Mobile/api_sustainability/lib/data/achievements.dart
@@ -0,0 +1,44 @@
+const achievements = {
+  "ID2": {
+    "0": 10,
+    "First Place!": "Getting Started",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "Starting your journey in a greener life!",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 10"
+  },
+  "ID3": {
+    "0": 20,
+    "First Place!": "Learning!",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "Learning the steps towards a greener life!",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 11"
+  },
+  "ID4": {
+    "0": 30,
+    "First Place!": "Repeating the basics!",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "Repeat and master ",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 12"
+  },
+  "ID5": {
+    "0": 40,
+    "First Place!": "Greener Life",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "The start of a better life",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 13"
+  },
+  "ID6": {
+    "0": 50,
+    "First Place!": "Here you go",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "Better choices! Good job",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 14"
+  },
+  "ID7": {
+    "0": 60,
+    "First Place!": "Master of Greener Life",
+    "You are ranking First Place between your friends, congrats!.. I mean is not that your friends are using this app anyways xd":
+        "You are probably in the 99% percentile!",
+    "Rank #1 between your friends": "Save a total of (grams of carbon) 15"
+  }
+};
