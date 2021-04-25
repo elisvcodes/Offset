326aaddea16ceb26fa9ceb12a258d717540bc14a
elisvcodes <sultanoveli@outlook.com>
2021-04-24 22:14:53 -0400

    Single Product reducer

--- Web/src/_Actions/products.js
+++ Web/src/_Actions/products.js
@@ -4,3 +4,8 @@ export const getProducts = () => async (dispatch) => {
   const { data } = await API.getProducts();
   dispatch({ type: 'GET_PRODUCTS', payload: data });
 };
+
+export const getSingleProduct = (id) => async (dispatch) => {
+  const { data } = await API.getSingleProduct(id);
+  dispatch({ type: 'GET_PRODUCT', payload: data });
+};
