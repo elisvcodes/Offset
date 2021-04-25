4eff79cc91903ee83f0ad3ebf582ddd2b1a6fd9d
elisvcodes <sultanoveli@outlook.com>
2021-04-24 22:13:47 -0400

    slight modification

--- Web/src/pages/Products/index.js
+++ Web/src/pages/Products/index.js
@@ -3,9 +3,8 @@ import { Container, Card, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../../components/Layout';
 import { getProducts } from '../../_Actions/products';
-import Button from '../../components/UI/Buttons/index';
-import { ImPriceTags } from 'react-icons/im';
-import { GiGasMask, GiLifeBar } from 'react-icons/gi';
+
+import SingleProduct from './SingleProduct';
 
 export default function Products() {
   const dispatch = useDispatch();
@@ -23,34 +22,8 @@ export default function Products() {
               <Row className="mt-5">
                 {Object.keys(products).map((product, index) => {
                   return (
-                    <Col sm={4}>
-                      <Card className="mb-5">
-                        <Card.Img
-                          variant="top"
-                          src={products[product].image_url}
-                          style={{ height: '50vh', objectFit: 'contain' }}
-                        />
-                        <Card.Body>
-                          <Card.Title>Card Title</Card.Title>
-                          <Card.Text>
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                          </Card.Text>
-                          <p>
-                            <ImPriceTags />{' '}
-                            {`Price: $${products[product].price}`}
-                          </p>
-                          <p>
-                            <GiGasMask /> Degrades in:{' '}
-                            {`${products[product].bio_time} Years`}
-                          </p>
-                          <p>
-                            <GiLifeBar />{' '}
-                            {`Life Span ~ ${products[product].lifespam} Months`}
-                          </p>
-                          <Button text="Compare" />
-                        </Card.Body>
-                      </Card>
+                    <Col xs={12} sm={4} key={product}>
+                      <SingleProduct product={products[product]} />
                     </Col>
                   );
                 })}
