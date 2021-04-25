ea554632522f864a3ecb9085f4153f7d12690078
NelsonWang <wangnelson2@gmail.com>
2021-04-25 13:06:49 -0400

    Fluter | Tracker | No product is being tracked message

--- Mobile/api_sustainability/lib/widgets/products_list_cost.dart
+++ Mobile/api_sustainability/lib/widgets/products_list_cost.dart
@@ -113,159 +113,181 @@ class _ProductsListCostState extends State<ProductsListCost> {
       });
     }
 
-    return ListView(children: <Widget>[
-      VerticalSpace(),
-      Text(
-        'Tracked Items',
-        style: MyText.display3(context),
-        textAlign: TextAlign.center,
-      ),
+    bool isAnyTracking =
+        productsData.trackedCategories.length > 0 ? true : false;
 
-      Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Wrap(
-                spacing: 10,
-                children: tagChips,
+    return isAnyTracking
+        ? ListView(children: <Widget>[
+            VerticalSpace(),
+            Text(
+              'Tracked Items',
+              style: MyText.display3(context),
+              textAlign: TextAlign.center,
+            ),
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Wrap(
+                      spacing: 10,
+                      children: tagChips,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      GridView.builder(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(10.0),
-        itemCount: products.length,
-        itemBuilder: (ctx, i) {
-          // print(_totalConsumptionCostsPerYear);
+            GridView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10.0),
+              itemCount: products.length,
+              itemBuilder: (ctx, i) {
+                // print(_totalConsumptionCostsPerYear);
 
-          return ChangeNotifierProvider.value(
-            value: products[i],
-            child: ProductItem(),
-          );
-        },
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
-      VerticalSpace(
-        height: 40,
-      ),
-      // Text(
-      //               "Lifespan: ${_lifeTimeValue.toStringAsFixed(3)} (months)",
-      //               style: MyText.body2(context)),
-      Text(
-        "Tracked Items Data",
-        style: MyText.display1(context),
-        textAlign: TextAlign.center,
-      ),
-      VerticalSpace(),
-      Text(
-        "CO2 EMISSION",
-        style: MyText.subhead(context),
-        textAlign: TextAlign.center,
-      ),
+                return ChangeNotifierProvider.value(
+                  value: products[i],
+                  child: ProductItem(),
+                );
+              },
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+            ),
+            VerticalSpace(
+              height: 40,
+            ),
+            // Text(
+            //               "Lifespan: ${_lifeTimeValue.toStringAsFixed(3)} (months)",
+            //               style: MyText.body2(context)),
+            Text(
+              "Tracked Items Data",
+              style: MyText.display1(context),
+              textAlign: TextAlign.center,
+            ),
+            VerticalSpace(),
+            Text(
+              "CO2 EMISSION",
+              style: MyText.subhead(context),
+              textAlign: TextAlign.center,
+            ),
 
-      VerticalSpace(
-        height: 5,
-      ),
-      Padding(
-        padding: marginLeftOnly,
-        child: Text(
-          "Tracked Items CO2 Emission (grams/year)",
-          style: MyText.body1(context),
-        ),
-      ),
-      VerticalSpace(
-        height: 5,
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Per year: ${_totalConsumptionCostsPerYear.toStringAsFixed(2)},",
-            style: MyText.body1(context)),
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Per month: ${productsData.trackedTotalEmissionPerMonth.toStringAsFixed(2)}",
-            style: MyText.body1(context)),
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Per day: ${productsData.trackedTotalEmissionPerDay.toStringAsFixed(2)}",
-            style: MyText.body1(context)),
-      ),
-      Padding(
-        padding: marginLeftOnly,
-        child: Container(
-          width: 300.0,
-          height: 100.0,
-          child: new Sparkline(
-            data: co2EmissionData,
-            lineColor: Colors.grey[500],
-            fillMode: FillMode.below,
-            fillColor: Colors.grey[200],
-            pointsMode: PointsMode.all,
-            pointSize: 5.0,
-            pointColor: Colors.grey,
-          ),
-        ),
-      ),
-      VerticalSpace(
-        height: 40,
-      ),
-      Text(
-        "YOUR CHOICES vs GENERIC ",
-        style: MyText.subhead(context),
-        textAlign: TextAlign.center,
-      ),
-      VerticalSpace(
-        height: 5,
-      ),
-      Padding(
-        padding: marginLeftOnly,
-        child: Text(
-          "CO2 (g/year)",
-          style: MyText.body1(context),
-        ),
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Yours: ${_totalConsumptionCostsPerYear.toStringAsFixed(2)}",
-            style: MyText.body1(context)),
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Generic: ${productsData.trackedCategoriesGenericsEmissionPerYear.toStringAsFixed(2)}",
-            style: MyText.body1(context)),
-      ),
-      Padding(
-        padding: marginLeftDouble,
-        child: Text(
-            "Saved: ${(productsData.trackedCategoriesGenericsEmissionPerYear - _totalConsumptionCostsPerYear).toStringAsFixed(2)}",
-            style: MyText.body1(context)),
-      ),
+            VerticalSpace(
+              height: 5,
+            ),
+            Padding(
+              padding: marginLeftOnly,
+              child: Text(
+                "Tracked Items CO2 Emission (grams/year)",
+                style: MyText.body1(context),
+              ),
+            ),
+            VerticalSpace(
+              height: 5,
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Per year: ${_totalConsumptionCostsPerYear.toStringAsFixed(2)},",
+                  style: MyText.body1(context)),
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Per month: ${productsData.trackedTotalEmissionPerMonth.toStringAsFixed(2)}",
+                  style: MyText.body1(context)),
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Per day: ${productsData.trackedTotalEmissionPerDay.toStringAsFixed(2)}",
+                  style: MyText.body1(context)),
+            ),
+            Padding(
+              padding: marginLeftOnly,
+              child: Container(
+                width: 300.0,
+                height: 100.0,
+                child: new Sparkline(
+                  data: co2EmissionData,
+                  lineColor: Colors.grey[500],
+                  fillMode: FillMode.below,
+                  fillColor: Colors.grey[200],
+                  pointsMode: PointsMode.all,
+                  pointSize: 5.0,
+                  pointColor: Colors.grey,
+                ),
+              ),
+            ),
+            VerticalSpace(
+              height: 40,
+            ),
+            Text(
+              "YOUR CHOICES vs GENERIC ",
+              style: MyText.subhead(context),
+              textAlign: TextAlign.center,
+            ),
+            VerticalSpace(
+              height: 5,
+            ),
+            Padding(
+              padding: marginLeftOnly,
+              child: Text(
+                "CO2 (g/year)",
+                style: MyText.body1(context),
+              ),
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Yours: ${_totalConsumptionCostsPerYear.toStringAsFixed(2)}",
+                  style: MyText.body1(context)),
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Generic: ${productsData.trackedCategoriesGenericsEmissionPerYear.toStringAsFixed(2)}",
+                  style: MyText.body1(context)),
+            ),
+            Padding(
+              padding: marginLeftDouble,
+              child: Text(
+                  "Saved: ${(productsData.trackedCategoriesGenericsEmissionPerYear - _totalConsumptionCostsPerYear).toStringAsFixed(2)}",
+                  style: MyText.body1(context)),
+            ),
 
-      Padding(
-        padding: marginLeftOnly,
-        child: SizedBox(
-          height: 150,
-          child: chartDisplayEmissions,
-        ),
-      ),
-      VerticalSpace(
-        height: 40,
-      )
-    ]);
+            Padding(
+              padding: marginLeftOnly,
+              child: SizedBox(
+                height: 150,
+                child: chartDisplayEmissions,
+              ),
+            ),
+            VerticalSpace(
+              height: 40,
+            )
+          ])
+        : ListView(children: <Widget>[
+            VerticalSpace(
+              height: 40,
+            ),
+            Text(
+              'No product is being tracked.',
+              style: MyText.headline(context),
+              textAlign: TextAlign.center,
+            ),
+            VerticalSpace(),
+            Padding(
+              padding: EdgeInsets.only(left: 40, right: 40),
+              child: Text(
+                "To add a product go to marketplace and tap on the tracking icon.",
+                style: MyText.body1(context),
+              ),
+            ),
+          ]);
   }
 }
