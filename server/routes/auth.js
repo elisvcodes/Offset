59304cdaae336c3749add47f2198904e6b7f70e9
elisvcodes <sultanoveli@outlook.com>
2021-07-21 11:15:20 -0400

    slight change

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { logIn, logOut } = require('../controller/auth');
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 
-router.post('/login', logIn);
 router.post('/logout', auth, logOut);
+router.post('/login', logIn);
 module.exports = router;
