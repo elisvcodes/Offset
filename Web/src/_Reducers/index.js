d2ed53e39bab655afce6ad13e09ac257b0ada117
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:29:24 -0400

    saved Products reducer

--- Web/src/_Reducers/index.js
+++ Web/src/_Reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 import products from './products';
 import product from './product';
 import auth from './auth';
+import savedProducts from './savedProducts';
+
 export default combineReducers({
   products,
   product,
+  savedProducts,
   auth,
 });
