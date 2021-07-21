b5907af2460a53690676096e9a027c8f5a4ecfc5
elisvcodes <sultanoveli@outlook.com>
2021-07-21 11:15:49 -0400

    some changes to the route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 
-const {
-  getSingleUser,
-  createUser,
-  getUserProfile,
-} = require('../controller/user');
-const { saveItem } = require('../controller/items');
-router.get('/:id', getSingleUser);
+const { createUser, getUserProfile } = require('../controller/user');
+
+// router.get('/:id', getSingleUser);
 router.post('/', createUser);
-router.post('/profile', auth, getUserProfile);
+router.get('/profile', auth, getUserProfile);
 
-router.post('/save', auth, saveItem);
+// router.post('/save', auth, saveItem);
 
 module.exports = router;
