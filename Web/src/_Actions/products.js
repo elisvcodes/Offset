55c01891acfdf1ddbe3986ba6b4d1fce0762ace2
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:28:32 -0400

    added an action to get saved items

--- Web/src/_Actions/products.js
+++ Web/src/_Actions/products.js
@@ -9,3 +9,12 @@ export const getSingleProduct = (id) => async (dispatch) => {
   const { data } = await API.getSingleProduct(id);
   dispatch({ type: 'GET_PRODUCT', payload: data });
 };
+
+export const getSavedProducts = () => async (dispatch) => {
+  const { data } = await API.getSavedProducts();
+  dispatch({ type: 'SAVED_ITEMS', payload: data });
+};
+
+export const saveItem = (id) => async () => {
+  await API.saveItem(id);
+};
