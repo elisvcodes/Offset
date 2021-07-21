0e833c90ba0a9cb49161ae5f4947c5b0e39937e2
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:29:03 -0400

    added the ability to track user login status

--- Web/src/_Reducers/auth.js
+++ Web/src/_Reducers/auth.js
@@ -1,11 +1,13 @@
-export default (auth = { authData: null }, action) => {
+export default (auth = { authData: null, isLoggedIn: false }, action) => {
   switch (action.type) {
     case 'AUTH':
       localStorage.setItem('profile', JSON.stringify(action.payload));
-      return { ...auth, authData: action.payload };
+      return { ...auth, authData: action.payload, isLoggedIn: true };
+    case 'isLoggedIn':
+      return { ...auth, isLoggedIn: action.payload };
     case 'LOGOUT':
       localStorage.clear();
-      return { ...auth, authData: null };
+      return { ...auth, authData: null, isLoggedIn: false };
     default:
       return auth;
   }
