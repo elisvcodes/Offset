495dceb44719ff0a7c3bf749375d7fa1b014f3e0
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:27:32 -0400

    added extra routes and dispatches

--- Web/src/App.js
+++ Web/src/App.js
@@ -6,36 +6,34 @@ import Home from './pages/Home/index';
 import Products from './pages/Products';
 import SingleProductDashboard from './pages/Products/SingleProductDashboard';
 import NotFound from './pages/NotFound';
-
+import { getSavedProducts } from './_Actions/products';
+import { isLoggedIn } from './_Actions/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import PrivateRoute from './HOF/PrivateRoute';
 function App() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+
   useEffect(() => {
+    dispatch(isLoggedIn());
+
     if (localStorage.getItem('profile')) {
-      setUser(JSON.parse(localStorage.getItem('profile')));
-    } else {
-      setUser(null);
+      dispatch(getSavedProducts());
     }
-  }, []);
+  }, [dispatch]);
+
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route
-        path="/login"
-        render={(props) => <Auth {...props} setUser={setUser} />}
+      <Route exact path='/' component={Home} />
+      <Route path='/login' render={(props) => <Auth {...props} />} />
+      <Route path='/signup' render={(props) => <Auth {...props} />} />
+      <PrivateRoute
+        path='/profile'
+        render={(props) => <Profile {...props} />}
       />
+      <Route path='/products' render={(props) => <Products {...props} />} />
+
       <Route
-        path="/signup"
-        render={(props) => <Auth {...props} setUser={setUser} />}
-      />
-      <Route
-        path="/profile"
-        render={(props) => (
-          <Profile {...props} user={user !== null ? user.result : user} />
-        )}
-      />
-      <Route path="/products" component={Products} />
-      <Route
-        path="/product/:id"
+        path='/product/:id'
         render={(props) => <SingleProductDashboard {...props} />}
       />
       <Route component={NotFound} />
