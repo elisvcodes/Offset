050d1e7d74a38adf03a470eabd62c97fb60b5146
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:32:17 -0400

    redirect logged in users

--- Web/src/pages/Auth/index.js
+++ Web/src/pages/Auth/index.js
@@ -3,15 +3,15 @@ import { Form, Container } from 'react-bootstrap';
 import Layout from '../../components/Layout';
 import Input from '../../components/UI/Forms/Inputs/index';
 import Button from '../../components/UI/Buttons/index';
-import { useHistory } from 'react-router-dom';
-import { signUp, login } from '../../_Actions/auth';
-import { useDispatch } from 'react-redux';
+import { useHistory, Redirect } from 'react-router-dom';
+import { signUp, login, isLoggedIn } from '../../_Actions/auth';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Auth(props) {
   const { setUser, match } = props;
   const dispatch = useDispatch();
   const history = useHistory();
-
+  const auth = useSelector((state) => state.auth);
   let isJoin;
   if (match.path === '/signup') {
     isJoin = true;
@@ -35,12 +35,16 @@ export default function Auth(props) {
     }
   };
 
+  if (auth.isLoggedIn) {
+    return <Redirect to='/products' />;
+  }
+
   return (
     <>
       <Layout>
         <Container>
           <Form
-            className="mt-5"
+            className='mt-5'
             onSubmit={onSubmit}
             style={{
               maxWidth: '600px',
@@ -49,28 +53,31 @@ export default function Auth(props) {
           >
             {isJoin && (
               <Input
-                name="fullName"
+                type='text'
+                name='fullName'
                 value={input.fullName}
-                placeholder="Full Name"
+                placeholder='Full Name'
                 onChange={onChange}
-                label="Your Full Name"
+                label='Your Full Name'
               />
             )}
             <Input
-              name="email"
-              placeholder="Email"
+              type='email'
+              name='email'
+              placeholder='Email'
               value={input.email}
               onChange={onChange}
-              label="Email"
+              label='Email'
             />
             <Input
-              name="password"
-              placeholder="password"
+              type='password'
+              name='password'
+              placeholder='password'
               value={input.password}
               onChange={onChange}
-              label="Password"
+              label='Password'
             />
-            <Button text={isJoin ? 'Sign Up' : 'Login'} type="submit" />
+            <Button text={isJoin ? 'Sign Up' : 'Login'} type='submit' />
           </Form>
         </Container>
       </Layout>
