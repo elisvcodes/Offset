760576220eb82dbbd40de100a32a7c0b0ba3b954
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:30:44 -0400

    button component now listens to onClick

--- Web/src/components/UI/Buttons/index.js
+++ Web/src/components/UI/Buttons/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button as Btn } from 'react-bootstrap';
-export default function Button({ text, type }) {
+export default function Button({ text, type, onClick }) {
   return (
     <>
       <Btn
         type={type}
         style={{ background: '#5DB075', border: 'none', outline: 'none' }}
+        onClick={onClick}
       >
         {text}
       </Btn>
