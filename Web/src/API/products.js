cd131eb53c332ff7d92709e6675334a658d824d4
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:29:49 -0400

    twp extra apis for saved and getsaved

--- Web/src/API/products.js
+++ Web/src/API/products.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
 
+axios.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.authorization = `Bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`;
+  }
+  return req;
+});
+
 export const getProducts = () =>
   axios.get('https://hackathon-app.server.elisv.com/items/');
 
 export const getSingleProduct = (id) =>
   axios.get(`https://hackathon-app.server.elisv.com/items/${id}`);
+
+export const getSavedProducts = () =>
+  axios.get(`https://hackathon-app.server.elisv.com/items/saved`);
+
+export const saveItem = (data) =>
+  axios.post(`https://hackathon-app.server.elisv.com/items/save/${data}`);
