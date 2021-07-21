cd243880dc8c18b29bca270bc791280f60837af2
elisvcodes <sultanoveli@outlook.com>
2021-07-21 19:28:04 -0400

    cleaned up auth action

--- Web/src/_Actions/auth.js
+++ Web/src/_Actions/auth.js
@@ -1,20 +1,37 @@
 import * as API from '../API/auth';
 
 export const signUp = (user, history, setUser) => async (dispatch) => {
-  const { data } = await API.signUp(user);
-  dispatch({ type: 'AUTH', payload: data });
-  setUser(data);
-  history.push(`/profile`);
+  const { status, data } = await API.signUp(user);
+  if (localStorage.getItem('profile')) {
+    dispatch({ type: 'isLoggedIn', payload: false });
+  } else if (status == 200) {
+    dispatch({ type: 'AUTH', payload: data });
+    // setUser(data);
+  }
+  // history.push(`/profile`);
 };
 
 export const login = (creds, history, setUser) => async (dispatch) => {
-  const { data } = await API.login(creds);
-  dispatch({ type: 'AUTH', payload: data });
-  setUser(data);
-  history.push(`/profile`);
+  const { status, data } = await API.login(creds);
+  if (localStorage.getItem('profile')) {
+    dispatch({ type: 'isLoggedIn', payload: false });
+  } else if (status == 200) {
+    dispatch({ type: 'AUTH', payload: data });
+    // setUser(data);
+  }
+  // history.push(`/profile`);
 };
 
-export const logout = () => async (dispatch) => {
+export const isLoggedIn = () => async (dispatch) => {
+  if (localStorage.getItem('profile')) {
+    dispatch({ type: 'isLoggedIn', payload: true });
+  } else {
+    dispatch({ type: 'isLoggedIn', payload: false });
+  }
+};
+
+export const logout = (history) => async (dispatch) => {
   const { data } = await API.logout();
   dispatch({ type: 'LOGOUT', payload: data });
+  history.push('/login');
 };
